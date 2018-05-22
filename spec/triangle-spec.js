b06bb576f0cfb3f425d7da3644544e991564a6c9
Alex Noble and Brian Henry <alex+brian@>
2018-05-22 10:43:31 -0700

    add reusable triangle

--- spec/triangle-spec.js
+++ spec/triangle-spec.js
@@ -2,6 +2,16 @@ import { Triangle } from './../src/triangle.js';
 
 describe('Triangle', function() {
 
+  var reusableTriangle;
+
+  beforeEach(function(){
+    reusableTriangle = new Triangle(5,5,5);
+  });
+
+  it('should show how beforeEach() works', function(){
+    console.log(reusableTriangle);
+  });
+
   it('should test whether a Triangle has three sides', function() {
     var triangle = new Triangle(3,4,5);
     expect(triangle.side1).toEqual(3);
@@ -13,5 +23,16 @@ describe('Triangle', function() {
     var notTriangle = new Triangle(3,9,22);
     expect(notTriangle.checkType()).toEqual("not a triangle");
   });
+
+  it('should show how beforeEach() works', function() {
+    console.log(reusableTriangle);
+  });
+
+  // it('should test whether a triangle is an equilateral triangle', function(){
+  //   var equilateralTriangle=new Triangle(3,3,3);
+  //   expect(triangle.side1).toEqual(2);
+  //   expect(triangle.side2).toEqual(3);
+  //   expect(triangle.side3).toEqual(1);
+  // });
 
 });
--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { starter } from './triangletracker.js';
+import { triangle } from './triangletracker.js';
 
 $(document).ready(function() {
   $('#triangle-tracker-form').submit(function(event) {
     event.preventDefault();
     var goal = $('#goal').val();
-    var output = starter(goal);
+    var output = triangle(goal);
     output.forEach(function(element) {
       $('#solution').append("<li>" + element + "</li>");
     });
